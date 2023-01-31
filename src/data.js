52e744688380e5abc7da2da7b0a09da2379f8145
DayanMonteiro <dmsmamb@gmail.com>
2023-01-31 13:18:01 -0300

    component slide

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/9772525/pexels-photo-9772525.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "SUMMER SALE",
+    description:
+      "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/2420029/pexels-photo-2420029.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "AUTUMN COLLECTION",
+    description:
+      "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/2531090/pexels-photo-2531090.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "LOUNGEWEAR LOVE",
+    description:
+      "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
